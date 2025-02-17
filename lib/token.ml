6f97a5dec41c17c30e4f0f03759a5dd0d47f8f43
44mira <legolaslada@gmail.com>
2025-02-17 17:45:27 +0800

    chore: redo grammar

--- lib/token.ml
+++ lib/token.ml
@@ -2,25 +2,21 @@
   unquoted braces { }             => 0 or more occurrences
   pipe |                          => alternate
   brackets with 2 dots [ x .. y ] => range of literals from x to y, inclusive
+  brackets [ ]                    => 0 or 1 occurrence
   single-quoted symbols           => literals
 
   ** whitespace is ignored 
 
   Regex Grammar:
 
-  <expr>      ::= <term>
-              |   <alternate>
+  <expr>      ::= <term> { '+' <term> }
+  <term>      ::= <unit> { <unit> }
 
-  <alternate> ::= <term> '+' <term> { '+' <term> }
+  <unit>      ::= <primary> [ '*' ]
 
-  <term>      ::= <letter> { <letter> }
-              |   '(' <expr> ')'
-              |   <repeat>
-
-  <repeat>    ::= <term> '*'
-
-  <letter>    ::= ['a' .. 'z']
+  <primary>   ::= [a .. z]
               |   <LAMBDA>
+              |   '(' <expr> ')'
 
   <LAMBDA>    ::= 'L'
  *)
