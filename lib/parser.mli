f151c8f1abce18e4b6c88a20664790af58b98a5c
44mira <legolaslada@gmail.com>
2025-02-17 17:45:59 +0800

    feat: add parser.mli

--- lib/parser.mli
+++ lib/parser.mli
@@ -1,2 +1,10 @@
 (** Private parser type *)
 type t
+
+val init : Lexer.t -> t
+val parse : t -> (t * Node.expression, string) result
+
+(** Pretty printing *)
+
+val pp : Format.formatter -> t -> unit
+val show : t -> string
