3bebd41b86f6df9edf511f7eeba7c11b6f988be1
44mira <legolaslada@gmail.com>
2025-02-18 23:34:28 +0800

    feat: check valid regex

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,5 @@
+open Rgxx
+
 (** Command-line args *)
 
 let usage_msg =
@@ -7,8 +9,6 @@ let usage_msg =
 let regex_pattern = ref ""
 let test_file = ref ""
 let anon_fun _ = ()
-
-exception File_does_not_exist of string
 
 let speclist =
   [ ( "-p"
@@ -23,9 +23,17 @@ let () = Arg.parse speclist anon_fun usage_msg
 
 (** Main *)
 let () =
+  (* Check for file existence *)
   if not @@ Sys.file_exists !test_file then
-    raise @@ File_does_not_exist (!test_file ^ " is not a valid file")
-  else (
+    raise @@ File_does_not_exist (!test_file ^ " is not a valid file");
+
+  (* Check for valid regex pattern *)
+  let parse pattern = Parser.(parse @@ init @@ Lexer.init pattern) in
+  match parse !regex_pattern with
+  | Error e -> raise @@ Invalid_pattern e
+  | Ok (_, _ast) ->
+    (* Evaluate each line of textfile with respect to the abstract syntax tree 
+       from the regex pattern*)
     let inp = open_in !test_file in
-    List.iter print_endline @@ In_channel.input_lines inp)
+    List.iter print_endline @@ In_channel.input_lines inp
 ;;
